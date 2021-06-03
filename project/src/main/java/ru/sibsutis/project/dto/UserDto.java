3822b3c247df19f43c13a402336d11f0bf086bc8
MikeAirone <onishencko2011@yandex.ru>
2021-06-03 13:17:02 +0700

    Fix userdto

--- project/src/main/java/ru/sibsutis/project/dto/UserDto.java
+++ project/src/main/java/ru/sibsutis/project/dto/UserDto.java
@@ -2,24 +2,18 @@ package ru.sibsutis.project.dto;
 
 public class UserDto {
     private final String name;
-    private final String number;
 
     private final String email;
     private final String password;
 
-    public UserDto(String name, String number, String email, String password, boolean isInteractionPost, String address) {
+    public UserDto(String name, String email, String password) {
         this.name = name;
-        this.number = number;
         this.email = email;
         this.password = password;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getNumber() {
-        return number;
     }
 
     public String getEmail() {
