a745f787f244d9b9417f68adb17d53508ff4c92d
MikeAirone <onishencko2011@yandex.ru>
2021-06-05 13:50:10 +0700

    Add trades table

---
+++ project/src/main/java/ru/sibsutis/project/Trade.java
@@ -0,0 +1,54 @@
+package ru.sibsutis.project;
+
+import ru.sibsutis.project.databases.Product;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "trades")
+public class Trade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Product send;
+
+    @OneToOne
+    private Product receive;
+
+    private Instant dateAndTime;
+
+    public Trade(Product send, Product receive, Instant dateAndTime) {
+        this.send = send;
+        this.receive = receive;
+        this.dateAndTime = dateAndTime;
+    }
+
+    public Trade() {}
+
+    public Product getSend() {
+        return send;
+    }
+
+    public void setSend(Product send) {
+        this.send = send;
+    }
+
+    public Product getReceive() {
+        return receive;
+    }
+
+    public void setReceive(Product receive) {
+        this.receive = receive;
+    }
+
+    public Instant getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(Instant dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+}
